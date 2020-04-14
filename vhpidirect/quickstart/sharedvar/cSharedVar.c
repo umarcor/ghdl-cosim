12fc6628e1d9ba8d69fd04e39529dcffe46a79d7
RocketRoss <mr@radonn.co.za>
2020-04-14 06:48:35 +0200

    cSharedVar.c -> cSharedVar.h

--- vhpidirect/quickstart/sharedvar/cSharedVar.c
+++
@@ -1,9 +0,0 @@
-#include "cSharedVar.h"
-
-int* getInt_ptr(){
-    return &var;
-}
-
-void printInt(){
-    printf("C-side print of int: %d\n", var);
-}
--- vhpidirect/quickstart/sharedvar/cSharedVar.h
+++ vhpidirect/quickstart/sharedvar/cSharedVar.h
@@ -2,5 +2,10 @@
 
 int var;
 
-int* getInt_ptr();
-void printInt();
+int* getInt_ptr(){
+    return &var;
+}
+
+void printInt(){
+    printf("C-side print of int: %d\n", var);
+}
