938e04f6b6bb0acc0e27803c8a4f7ff9c044db74
RocketRoss <mr@radonn.co.za>
2020-04-15 11:20:05 +0200

    Renames int_ptr_prot and ent_93.vhd, add -frelaxed

--- vhpidirect/quickstart/sharedvar/run.sh
+++ vhpidirect/quickstart/sharedvar/run.sh
@@ -4,8 +4,8 @@ set -e
 
 cd $(dirname "$0")
 
-echo "VHDL 93: Analyze pkg_93.vhd ent.vhd tb.vhd"
-ghdl -a --std=93 pkg_93.vhd ent.vhd tb.vhd
+echo "VHDL 93: Analyze pkg_93.vhd ent_93.vhd tb.vhd"
+ghdl -a --std=93 pkg_93.vhd ent_93.vhd tb.vhd
 
 echo "Build tb_93 with main.c"
 ghdl -e --std=93 -Wl,main.c -o tb_93 tb
@@ -15,7 +15,7 @@ echo "Execute tb_93"
 
 echo "Clean tb_93"
 rm work-obj*.cf tb_93 *.o
-
+echo ""
 
 
 echo "VHDL 08: Analyze pkg_08.vhd ent_08.vhd tb.vhd"
@@ -29,3 +29,17 @@ echo "Execute tb_08"
 
 echo "Clean tb_08"
 rm work-obj*.cf tb_08 *.o
+echo ""
+
+
+echo "VHDL 08 -frelaxed: Analyze pkg_93.vhd ent_93.vhd tb.vhd"
+ghdl -a --std=08 -frelaxed pkg_93.vhd ent_93.vhd tb.vhd
+
+echo "Build tb_08relaxed with main.c"
+ghdl -e --std=08 -frelaxed -Wl,main.c -o tb_08relaxed tb
+
+echo "Execute tb_08relaxed"
+./tb_08relaxed
+
+echo "Clean tb_08relaxed"
+rm work-obj*.cf tb_08relaxed *.o
