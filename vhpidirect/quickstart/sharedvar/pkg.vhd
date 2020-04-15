6037a36c12bb18d6ca43cb74df21e6369cdc8576
RocketRoss <mr@radonn.co.za>
2020-04-15 08:12:09 +0200

    Merge ent1/2 to ent

--- vhpidirect/quickstart/sharedvar/pkg.vhd
+++ vhpidirect/quickstart/sharedvar/pkg.vhd
@@ -2,9 +2,9 @@ package pkg is
 	type int_ptr is access integer;
 	
 	function c_Int_ptr return int_ptr;
-		attribute foreign of c_Int_ptr : function is "VHPIDIRECT getInt_ptr";
+	attribute foreign of c_Int_ptr : function is "VHPIDIRECT getInt_ptr";
 	procedure c_printVar;
-		attribute foreign of c_printVar : procedure is "VHPIDIRECT printInt";
+	attribute foreign of c_printVar : procedure is "VHPIDIRECT printInt";
 
 	shared variable c_Var : int_ptr := c_Int_ptr;	
 end package pkg;
