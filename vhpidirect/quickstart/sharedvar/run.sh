3aa7d1b10d7e930174761878fb3792544d7dc0b6
RocketRoss <mr@radonn.co.za>
2020-04-15 08:16:33 +0200

    Tage as VHDL 93

--- vhpidirect/quickstart/sharedvar/run.sh
+++ vhpidirect/quickstart/sharedvar/run.sh
@@ -4,11 +4,11 @@ set -e
 
 cd $(dirname "$0")
 
-echo "Analyze pkg.vhd ent.vhd tb.vhd"
-ghdl -a pkg.vhd ent.vhd tb.vhd
+echo "VHDL 93: Analyze pkg_93.vhd ent.vhd tb.vhd"
+ghdl -a --std=93 pkg_93.vhd ent.vhd tb.vhd
 
-echo "Build tb with main.c"
-ghdl -e -Wl,main.c tb
+echo "Build tb_93 with main.c"
+ghdl -e --std=93 -Wl,main.c -o tb_93 tb
 
-echo "Execute tb"
-./tb
+echo "Execute tb_93"
+./tb_93
