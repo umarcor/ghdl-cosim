abe6a7ffc469375c7e83fd3407aaa26affac488e
RocketRoss <code@radonn.co.za>
2020-04-17 13:42:15 +0200

    caux

--- vhpidirect/cinterface/accarraygen/run.sh
+++ vhpidirect/cinterface/accarraygen/run.sh
@@ -7,8 +7,8 @@ cd $(dirname "$0")
 echo "Analyze pkg.vhd and tb.vhd"
 ghdl -a --std=08 pkg.vhd tb.vhd
 
-echo "Build tb (with aux.c) [GHDL]"
-ghdl -e -Wl,aux.c --std=08 tb
+echo "Build tb (with caux.c) [GHDL]"
+ghdl -e -Wl,caux.c --std=08 tb
 
 echo "Execute tb (-gArraySize=5)"
 ./tb -gArraySize=5
--- vhpidirect/cinterface/accarrayint/run.sh
+++ vhpidirect/cinterface/accarrayint/run.sh
@@ -7,8 +7,8 @@ cd $(dirname "$0")
 echo "Analyze pkg.vhd tb.vhd"
 ghdl -a pkg.vhd tb.vhd
 
-echo "Build tb (with co-op.c) [GHDL]"
-ghdl -e -Wl,co-op.c tb
+echo "Build tb (with caux.c) [GHDL]"
+ghdl -e -Wl,caux.c tb
 
 echo "Execute tb"
 ./tb
