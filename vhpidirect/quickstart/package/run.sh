d9a7af9bcac6ccdf845d7319a5b12d8487a5d41a
RocketRoss <code@radonn.co.za>
2020-04-17 10:31:40 +0200

    caux and white spaces

--- vhpidirect/quickstart/package/run.sh
+++ vhpidirect/quickstart/package/run.sh
@@ -7,8 +7,8 @@ cd $(dirname "$0")
 echo "Analyze pkg.vhd ent.vhd tb.vhd"
 ghdl -a pkg.vhd ent.vhd tb.vhd
 
-echo "Build tb with main.c"
-ghdl -e -Wl,main.c tb
+echo "Build tb with caux.c"
+ghdl -e -Wl,caux.c tb
 
 echo "Execute tb"
 ./tb
--- vhpidirect/quickstart/package/tb.vhd
+++ vhpidirect/quickstart/package/tb.vhd
@@ -4,13 +4,12 @@ end tb;
 architecture arch of tb is
 begin
 
-  entA : entity work.ent(rtl_A);
-  entB : entity work.ent(rtl_B);
+	entA : entity work.ent(rtl_A);
+	entB : entity work.ent(rtl_B);
 
-  process
-  begin
-    report "Testbench." severity note;
-    wait;
-  end process;
-
+	process
+	begin
+		report "Testbench." severity note;
+		wait;
+	end process;
 end;
